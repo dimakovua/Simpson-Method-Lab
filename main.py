27205b3d00f7c239d3362e09e3f6be28ba00b2cf
Dmytro Kovalenko <dimitri.yu.kovalenko@gmail.com>
2022-10-03 16:31:02 +0300

    main func and matplotlib

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+from matplotlib import pyplot as plt
 import numpy as np
 
+
 left = 0
-right = np.pi/2
+right = np.pi/2 - 0.00001
 step = np.pi/36
+#1.570796326794896
+#1.570796326794896
 points = []
+eps = 0.00001
+prev_approx = 2
+a = 0
+b = 0
+
+
+def runge_rule(right_end):
+    global prev_approx
+    print(prev_approx)
+    pieces = prev_approx
+    global a
+    global b
+    a = simpson_method(right_end, pieces)
+    b = simpson_method(right_end, pieces / 2)
+    print("in")
+    while np.abs(a - b)/15 >= eps:
+        #print(1/15 * np.abs(a - b))
+        pieces += 2
+        a = simpson_method(right_end, pieces)
+        b = simpson_method(right_end, pieces/2)
+    print("out")
+    prev_approx = pieces
+    return a
 
 
 def simpson_method(right_end, num_of_pieces):
+    print(f"Right end = {right_end}, num = {num_of_pieces}" )
     local_step = (right_end-left)/num_of_pieces
-    print(local_step)
+    #print(local_step)
     array_of_even = [] #парні х
     array_of_odd = [] #непарні х
     i = 0
@@ -20,19 +48,19 @@ def simpson_method(right_end, num_of_pieces):
         else:
             array_of_even.append(temp)
         i = i + 1
-    print(array_of_even)
-    print(array_of_odd)
+    #print(array_of_even)
+    #print(array_of_odd)
 
     sum_for_even = 0
     for e in array_of_even:
 
         sum_for_even += 2*func(e)
-        print(f"for x = {e} func is {4*func(e)}")
+        #print(f"for x = {e} func is {4*func(e)}")
 
     sum_for_odd = 0
     for o in array_of_odd:
         sum_for_odd += 4*func(o)
-        print(f"for x = {o} func is {4*func(o)}")
+        #print(f"for x = {o} func is {4*func(o)}")
 
     return local_step/3 * (func(left) + func(right_end) + sum_for_odd + sum_for_even)
 
@@ -41,9 +69,30 @@ def func(x):
     return np.log(np.cos(x))
 
 
+def func1(x):
+    return (-1) * np.log(np.cos(x))
+
+
+def do_staff():
+    res = {}
+    resv = []
+    for ele in points:
+        k = (-1)*runge_rule(ele)
+        resv.append(k)
+        res[ele] = k
+
+    print(res)
+    # functional_table = np.append(functional_table, [[points, resv]], axis=0)
+    # print(f"{functional_table=}")
+    # # x, y = functional_table.T
+    plt.plot(points, resv)
+    plt.show()
+
+
 def fill_points():
-    i = left
-    while i <= right:
+    i = left + step
+    print(right)
+    while i < right:
         points.append(i)
         i = i + step
     print(points)
@@ -51,4 +100,5 @@ def fill_points():
 
 if __name__ == '__main__':
     fill_points()
-    print(simpson_method(np.pi/4, 18))
+    #print(simpson_method(np.pi/2-0.001, 2230))
+    do_staff()
